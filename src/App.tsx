2bb834678bc1c878083082b094027b87f882d615
nurguanadeeva <enee.kors@mail.ru>
2022-10-18 11:32:34 +0300

    added component and button as child component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card, CardVariant } from "./components/Card";
+import { log } from "util";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => {
+    return (
+        <div>
+            <Card
+                height='200px'
+                width='200px'
+                variant={CardVariant.primary}
+                onClick={(num) => console.log('click', num)}
+            >
+                <button>BTN</button>
+            </Card>
+        </div>
+    );
+};
---
+++ src/components/Card.tsx
@@ -0,0 +1,39 @@
+import React, { FC, useState } from 'react';
+
+export enum CardVariant {
+    outlined = 'outlined',
+    primary = 'primary'
+}
+
+interface CardProps {
+    width?: string,
+    height?: string,
+    children: React.ReactNode | React.ReactChild,
+    variant: CardVariant,
+    onClick: (num: number) => void
+}
+
+export const Card: FC<CardProps> =
+    ({
+         width,
+         height,
+         children,
+         variant,
+        onClick
+     }) => {
+    const [state, setState] = useState(0)
+        return (
+            <div style=
+                     {{
+                         width,
+                         height,
+                         border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
+                         background: variant === CardVariant.primary ? 'lightgray' : ''
+                     }}
+                 onClick={() => onClick(state)}
+            >
+                {children}
+            </div>
+        );
+    };
+
